73128f18b2fe1432fd517ba4d11ae141131dcb1d
cmanaog <ChristianManaog@ChristiansMBP2.wv.cc.cmu.edu>
2017-12-10 00:54:37 -0500

    init_commit

---
+++ hello_aliens.R
@@ -0,0 +1,10 @@
+cat("Hello aliens. My name is Christian Manaog. I have been selected on behalf
+    of earth to be the spokeperson for this planet. There are a lot of things 
+    that are imperfections on this planet. If you're hearing this, then at this
+    very moment, millions of us are dying from starvation, disease, and war. 
+    We are living on the edge of oblivion as we are only short misunderstandings
+    away from completely destroying our planet. But the human race has a saving
+    grace, a perfect God whose mercy has saved us from death. And if He has put
+    life on other planets, then you too are a part of His great plan. So even
+    though there is evil on this planet, know that you will be welcomed and 
+    accepted like any other being on this planet. We'll be waiting.")
